18dfa3d1ff0319558565b9fba296987eaab5eee1
chanchai-pom <chanchai.c@techcombine.co>
2025-03-19 20:51:54 +0700

    feat: add go doc

--- add.go
+++ add.go
@@ -1,5 +1,8 @@
+// this is the first public module for add two number
 package add
 
+// An Add function is use for add a and b to an output
+// see more https://www.mathsisfun.com/numbers/addition.html
 func Add(a, b int) int {
 	return a + b
 }
